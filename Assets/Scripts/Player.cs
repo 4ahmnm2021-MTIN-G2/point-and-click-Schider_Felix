933540f4c3e0207c690d8484d4c579337e78a173
schiderf <felix.schider02@htl-salzburg.ac.at>
2021-04-11 22:30:46 +0200

    update player movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public Transform thisTransform;
     public Vector3 mousePos;
-    public Vector3 mouseCameraPos;
+    public Vector3 worldPos;
+    public Vector3 playerPos;
+    public float v = 0.1f;
 
+    void Start ()
+    {
+        playerPos = thisTransform.position;
+    }
+    
+    
     void Update()
     {
-        if (Input.GetMouseButton(0))  {
+        if (Input.GetMouseButton(0))  
+        {
             mousePos = Input.mousePosition;
-            mouseCameraPos = Camera.main.ScreenToWorldPoint(mousePos);
-            this.transform.position = mouseCameraPos;
+            worldPos = Camera.main.ScreenToWorldPoint(mousePos);
+            playerPos = new Vector3(worldPos.x, worldPos.y, thisTransform.position.z);
         }
+
+        thisTransform.position = Vector3.MoveTowards(thisTransform.position, playerPos, v);
+
     }
 }
