37a4af6bc8c6a464cc0438c45aa61d35863d89b5
schiderf <felix.schider02@htl-salzburg.ac.at>
2021-04-18 22:43:26 +0200

    create interactable objects, including Ui

---
+++ Assets/Scripts/InteractableObject.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InteractableObject : MonoBehaviour
+{
+    public GameObject interactableObj;
+    public Text Text;
+    public string TextString;
+
+
+    private void OnMouseDown()
+    {
+        interactableObj.SetActive(false);
+        Text.text = TextString;
+    }
+
+}
