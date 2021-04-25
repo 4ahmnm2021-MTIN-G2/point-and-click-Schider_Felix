7bda580e600e7aa3244d717254677ddf04ad3c67
schiderf <felix.schider02@htl-salzburg.ac.at>
2021-04-25 21:56:28 +0200

    add new features in samplescene

---
+++ Assets/Scripts/Buttonscrpt.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Buttonscrpt : MonoBehaviour
+{
+    public Button yourButton;
+    public GameObject Coco;
+
+    void Start()
+    {
+        Button btn = yourButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        Coco.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/treescrpt.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class treescrpt : MonoBehaviour
+{
+    public GameObject UiButton;
+
+    public Text Text;
+    public string TextString;
+
+    private void Start()
+    {
+        UiButton.SetActive(false);
+
+    }
+
+
+    private void OnMouseDown()
+    {
+        Text.text = TextString;
+        UiButton.SetActive(true);
+    }
+
+
+}
